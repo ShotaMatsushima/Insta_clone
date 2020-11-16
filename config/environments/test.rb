fb0428c4362f2c8224875466e00008dd96ec45e7
Shota Matsushima <shota19890523.engineer@gamil.com>
2020-11-16 06:08:44 +0000

    set again amazon-s3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.cache_classes = false
-  config.action_view.cache_template_loading = true
+  #config.action_view.cache_template_loading = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
 
   # Store uploaded files on the local file system in a temporary directory.
-  config.active_storage.service = :local
+  config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
 
