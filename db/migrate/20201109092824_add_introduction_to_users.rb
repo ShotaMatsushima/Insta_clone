72c596761b8185cd355a29f12fa5da4dd4f3b3f6
Shota Matsushima <shota19890523.engineer@gamil.com>
2020-11-17 06:22:50 +0000

    set again amazon-s3

--- db/migrate/20201109092824_add_introduction_to_users.rb
+++ db/migrate/20201109092824_add_introduction_to_users.rb
@@ -1,4 +1,4 @@
-class AddIntroductionTousers < ActiveRecord::Migration[6.0]
+class AddIntroductionToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :introduction, :text
   end
